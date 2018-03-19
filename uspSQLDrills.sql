67c7985b6749854b1251a29d98f03ea4fcdf0358
vespertron <vesper.annstas@gmail.com>
2018-03-19 14:01:09 -0700

    Update uspSQLDrills.sql

--- uspSQLDrills.sql
+++ uspSQLDrills.sql
@@ -1,22 +1,38 @@
 USE AdventureWorks
 GO
 
-CREATE PROCEDURE dbo.SQLDrills
-AS
+==============================================
+-- Drop stored procedure if it already exists.
+==============================================
+IF EXISTS (
+  SELECT * 
+    FROM INFORMATION_SCHEMA.ROUTINES 
+   WHERE SPECIFIC_SCHEMA = N'dbo'
+     AND SPECIFIC_NAME = N'SQLDrills
+)
+   DROP PROCEDURE dbo.SQLDrills
+GO
 
+==========================
+-- Create stored procedure
+==========================
+CREATE PROCEDURE dbo.SQLDrills
 /* Testing answer from bonskijr
 https://dba.stackexchange.com/questions/201623/ssms-query-variables-from-table-design-mode-column-description
 */
 
+--"WITH" means:
 ;WITH src
 AS (
     --#1 get extended info description meta attribute
     SELECT sys.objects.name                AS TableName,
            sys.columns.name                AS ColumnName,
            ep.name                         AS PropertyName,
+           --"CAST" means:
            CAST(ep.value AS NVARCHAR(255)) AS Description
     FROM sys.objects
         INNER JOIN sys.columns ON sys.objects.object_id = sys.columns.object_id
+        --"CROSS APPLY" means:
         CROSS APPLY fn_listextendedproperty(
                                                DEFAULT,
                                                'SCHEMA',
@@ -25,7 +41,7 @@ AS (
                                                sys.objects.name,
                                                'COLUMN',
                                                sys.columns.name
-                                           ) ep
+                                           ) ep --"ep" means:
     WHERE sys.objects.name = 'SalesOrderHeader' AND
           sys.columns.name = 'Status'
 ),
@@ -37,6 +53,7 @@ src2 AS (
  )
    --#3 extract id and description
 SELECT CAST(SUBSTRING(x.StatusDesc, 1, CHARINDEX('=', x.StatusDesc) - 1) AS TINYINT) AS StatusId,
+       --"LTRIM" and "RTRIM" mean:
        LTRIM(RTRIM(SUBSTRING(x.StatusDesc, CHARINDEX('=', x.StatusDesc) + 1, 255)))  AS StatusDesc
 FROM
 (
@@ -49,4 +66,10 @@ FROM
     )                                           t
         CROSS APPLY x.nodes('/XMLRoot/RowData') m(n)
 ) x;
+GO
+
+===========================
+-- Execute stored procedure
+===========================
+EXEC dbo.SQLDrills
 GO
