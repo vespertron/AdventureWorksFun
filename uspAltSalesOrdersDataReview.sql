3b1c432e03cbd77eedf71b3acaf6d90fcb286331
vespertron <vesper.annstas@gmail.com>
2018-03-19 13:49:09 -0700

    Update uspAltSalesOrdersDataReview.sql

--- uspAltSalesOrdersDataReview.sql
+++ uspAltSalesOrdersDataReview.sql
@@ -1,7 +1,28 @@
 USE AdventureWorks
 GO
 
+==============================================
+-- Drop stored procedure if it already exists.
+==============================================
+IF EXISTS (
+  SELECT * 
+    FROM INFORMATION_SCHEMA.ROUTINES 
+   WHERE SPECIFIC_SCHEMA = N'dbo'
+     AND SPECIFIC_NAME = N'AltSalesOrdersDataReview'
+)
+   DROP PROCEDURE dbo.AltSalesOrdersDataReview
+GO
+
+==========================
+-- Create stored procedure
+==========================
 CREATE PROCEDURE dbo.AltSalesOrdersDataReview
 AS
 
+GO
+
+============================
+-- Execute stored procedure.
+============================
+EXECUTE dbo.AltSalesOrdersDataReview
 GO
