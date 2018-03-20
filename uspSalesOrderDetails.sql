7c3d01ad4b9ed77e93efc97bf85105638e152f79
vespertron <vesper.annstas@gmail.com>
2018-03-20 16:36:37 -0700

    Change to Inner Join

--- uspSalesOrderDetails.sql
+++ uspSalesOrderDetails.sql
@@ -1,61 +1,58 @@
-USE AdventureWorks
+USE [AdventureWorks]
+GO
+/****** Object:  StoredProcedure [dbo].[SalesOrderDetails]    Script Date: 3/20/2018 9:23:49 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
 GO
 
--- Drop stored procedure if it already exists.
-IF EXISTS (
-  SELECT * 
-    FROM INFORMATION_SCHEMA.ROUTINES 
-   WHERE SPECIFIC_SCHEMA = N'dbo'
-     AND SPECIFIC_NAME = N'SalesOrderDetails'
-)
-   DROP PROCEDURE dbo.SalesOrderDetails
-GO
-
--- Create stored procedure
-CREATE PROCEDURE dbo.SalesOrderDetails
--- Set input param defaults so that proc executes with or without user input
-  @BeginDate DATE = NULL,
-  @EndDate DATE = NULL
+ALTER PROCEDURE [dbo].[SalesOrderDetails] 
+	/* Set input param defaults to NULL so that proc executes with or without user input */
+    @BeginDate DATE = NULL,
+    @EndDate DATE = NULL
 AS
-  SET @BeginDate = ISNULL(@BeginDate, GETDATE())
-  SET @EndDate = ISNULL(@EndDate, GETDATE())
+BEGIN
+	/* SET NOCOUNT ON added to prevent extra result sets from
+	interfering with SELECT statements. */
+	SET NOCOUNT ON;
+
+	SET @BeginDate = ISNULL(@BeginDate, GETDATE())
+    SET @EndDate = ISNULL(@EndDate, GETDATE())
   
-SELECT top 100
-  SH.SalesOrderID
-  ,SH.RevisionNumber
-  ,SH.OrderDate
-  ,SS.Status AS OrderStatusText
-  ,SH.SalesOrderNumber
-  ,SH.SubTotal
-  ,SH.TaxAmt
-  ,SH.Freight
-  ,SH.TotalDue
--- convert INT to STRING with 4 characters
-  ,RIGHT('000' + CONVERT(VARCHAR(4), SD.OrderQty),4) AS OrderQtyText
-  ,SD.UnitPrice
-  ,SD.UnitPriceDiscount
-  ,SD.LineTotal
-  ,PA.City AS ShipToCity
-  ,PSC.StateProvinceCode AS ShipToState
-  ,PA.PostalCode AS ShipToZip
-  ,PSC.CountryRegionCode AS ShipToCountryCode
-  ,PSC.CountryRegionName AS ShipToCountryName
-  ,PM.Name AS ModelName
-  ,PC.Name AS CategoryName
-  FROM Sales.SalesOrderHeader SH
-    LEFT OUTER JOIN Sales.SalesOrderStatus SS ON SH.Status = SS.StatusID
-    LEFT OUTER JOIN Sales.SalesOrderDetail SD ON SH.SalesOrderID = SD.SalesOrderID
-    LEFT OUTER JOIN Person.Address PA ON SH.ShipToAddressID = PA.AddressID
-    LEFT OUTER JOIN Person.vStateProvinceCountryRegion PSC ON PA.StateProvinceID = PSC.StateProvinceID
-    LEFT OUTER JOIN Production.Product PP ON SD.ProductID = PP.ProductID
-    LEFT OUTER JOIN Production.ProductModel PM ON PP.ProductModelID = PM.ProductModelID
-    LEFT OUTER JOIN Production.ProductSubcategory PS ON PP.ProductSubcategoryID = PS.ProductSubcategoryID
-    LEFT OUTER JOIN Production.ProductCategory PC ON PS.ProductCategoryID = PC.ProductCategoryID
-  WHERE SH.OrderDate BETWEEN @BeginDate AND @EndDate
-  ORDER BY SalesOrderNumber
-GO
+    SELECT top 100
+      SH.SalesOrderID
+      ,SH.RevisionNumber
+      ,SH.OrderDate
+      ,SS.Status AS OrderStatusText
+      ,SH.SalesOrderNumber
+      ,SH.SubTotal
+      ,SH.TaxAmt
+      ,SH.Freight
+      ,SH.TotalDue
+      /* convert INT to STRING with 4 characters */
+      ,RIGHT('000' + CONVERT(VARCHAR(4), SD.OrderQty),4) AS OrderQtyText
+      ,SD.UnitPrice
+      ,SD.UnitPriceDiscount
+      ,SD.LineTotal
+      ,PA.City AS ShipToCity
+      ,PSC.StateProvinceCode AS ShipToState
+      ,PA.PostalCode AS ShipToZip
+      ,PSC.CountryRegionCode AS ShipToCountryCode
+      ,PSC.CountryRegionName AS ShipToCountryName
+      ,PM.Name AS ModelName
+      ,PC.Name AS CategoryName
+    FROM Sales.SalesOrderHeader SH
+      INNER JOIN Sales.SalesOrderStatus SS ON SH.Status = SS.StatusID
+      INNER JOIN Sales.SalesOrderDetail SD ON SH.SalesOrderID = SD.SalesOrderID
+      INNER JOIN Person.Address PA ON SH.ShipToAddressID = PA.AddressID
+      INNER JOIN Person.vStateProvinceCountryRegion PSC ON PA.StateProvinceID = PSC.StateProvinceID
+      INNER JOIN Production.Product PP ON SD.ProductID = PP.ProductID
+      INNER JOIN Production.ProductModel PM ON PP.ProductModelID = PM.ProductModelID
+      INNER JOIN Production.ProductSubcategory PS ON PP.ProductSubcategoryID = PS.ProductSubcategoryID
+      INNER JOIN Production.ProductCategory PC ON PS.ProductCategoryID = PC.ProductCategoryID
+    WHERE SH.OrderDate BETWEEN @BeginDate AND @EndDate
+    ORDER BY SalesOrderNumber
+END
 
-
--- Execute stored procedure with or without input parameter entry
 EXECUTE dbo.SalesOrderDetails @BeginDate = '', @EndDate = ''
 GO
