80ea3244d2a44ac4474bb7db90fe022be37d16b6
vespertron <vesper.annstas@gmail.com>
2018-03-21 13:11:53 -0700

    Add SELECT stmt to uspSalesOrderDataReview.sql

--- uspSalesOrdersDataReview.sql
+++ uspSalesOrdersDataReview.sql
@@ -1,9 +1,7 @@
 USE AdventureWorks
 GO
 
-==============================================
 -- Drop stored procedure if it already exists.
-==============================================
 IF EXISTS (
   SELECT * 
     FROM INFORMATION_SCHEMA.ROUTINES 
@@ -13,16 +11,27 @@ IF EXISTS (
    DROP PROCEDURE dbo.SalesOrdersDataReview
 GO
 
-==========================
+
 -- Create stored procedure
-==========================
+
 CREATE PROCEDURE dbo.SalesOrdersDataReview
 AS
-
+ SELECT
+   SH.SalesOrderID
+   ,SalesOrderNumber
+   ,SubTotal AS OriginalSubtotal
+   ,SUM(LineTotal) AS CalculatedSubtotalFromDetail
+   ,DIFFERENCE(sum(SD.LineTotal), SH.SubTotal) AS Difference
+ FROM Sales.SalesOrderHeader SH
+  INNER JOIN Sales.SalesOrderDetail SD
+    ON SH.SalesOrderID = SD.SalesOrderID
+ WHERE DIFFERENCE(sum(SD.LineTotal), SH.SubTotal) > 0
+ GROUP BY SH.SalesOrderID
+  ,SalesOrderNumber
+  ,SubTotal
 GO
 
-============================
+
 -- Execute stored procedure.
-============================
 EXECUTE dbo.SalesOrdersDataReview
 GO
